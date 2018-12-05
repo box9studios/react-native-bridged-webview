7b708af30f6cc45bc358cfdfa74c3f84d07983ca
Steve Binder <email@stevebinder.com>
2018-12-05 17:09:06 -0500

    Move api surface to bridge folder

---
+++ bridge/index.js
@@ -0,0 +1,9 @@
+import call from './call';
+
+export default call;
+
+export { default as alert } from './alert';
+export { default as call } from './call';
+export { default as log } from './log';
+export { default as platform } from './platform';
+export { default as statusbar } from './statusbar';
